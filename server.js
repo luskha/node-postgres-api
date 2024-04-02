2a26ae83b505e0b15a6aea7c4429056e50b1d201
Angelo Lustosa de Figueiredo Filho <01337098337@DIGITALCOLLEGE.LOCAL>
2024-04-01 21:50:28 -0300

    Termino do crud

--- server.js
+++ server.js
@@ -28,7 +28,22 @@ app.get('/produtos', async (req, res) => {
     return result.rows;
 })
 
+app.get('/produto/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = `SELECT * FROM produtos where id = ${id}`
+    console.log(`[QUERY]: ${query}`);
+
+    const result = await config.query(query);
+
+    if(result.rows.length === 0) {
+        res.status(404).send(`Produto com o id: ${id} não encontrado!`)
+        return;
+    }
+    return result.rows;
+})
+
 app.post('/produto', async (req, res) => {
+    // adicionar condição para quando passar o id que não existe no banco
     const { nome, descricao, desconto, preco, ativo, categoria, data_cadastro } = req.body;
 
     try {
@@ -41,6 +56,45 @@ app.post('/produto', async (req, res) => {
         res.status(500).send('Erro ao inserir produto');
     }
 })
+
+// PUT para atualizar todos os campos de um produto
+app.put('/produto/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nome, descricao, desconto, preco, ativo, categoria, data_cadastro } = req.body;
+
+    try {
+        const query = 'UPDATE produtos SET nome=$1, descricao=$2, desconto=$3, preco=$4, ativo=$5, categoria=$6, data_cadastro=$7 WHERE id=$8 RETURNING *';
+        const values = Object.keys(req.body) //[nome, descricao, desconto, preco, ativo, categoria, data_cadastro, id];
+        const result = await config.query(query, values);
+        
+        if (result.rows.length === 0) {
+            res.status(404).send('Produto não encontrado');
+        } else {
+            res.send(result.rows[0]);
+        }
+    } catch (err) {
+        console.log('Erro ao atualizar produto:', err);
+        res.status(500).send('Erro ao atualizar produto');
+    }
+});
+
+app.delete('/produto/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const query = 'DELETE FROM produtos WHERE id=$1 RETURNING *';
+        const result = await config.query(query, [id]);
+
+        if (result.rows.length === 0) {
+            res.status(404).send('Produto não encontrado');
+        } else {
+            res.send('Produto excluído com sucesso');
+        }
+    } catch (err) {
+        console.log('Erro ao excluir produto:', err);
+        res.status(500).send('Erro ao excluir produto');
+    }
+});
 
 //app.register(rotasProdutos);
 
